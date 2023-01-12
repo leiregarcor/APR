418a0b1376c49786c4cfacd5d66810b7e38ba210
AitorSanJose1 <aitorsanjoseheras@gmail.com>
2023-01-12 20:48:44 +0100

    asda

--- proyectoRedes/plot.py
+++ proyectoRedes/plot.py
@@ -7,7 +7,7 @@ fig, ax = plt.subplots()
 ax.set_title('tasa de error segun el numero de neuronas');
 ax.grid();
 ax.set_xlim([10,2100]);
-ax.set_xlabel('Epoch');
+ax.set_xlabel('Neuronas');
 ax.set_ylim([7,14]);
 ax.set_ylabel('error de clasificación');
 ax.plot(d[:,0], d[:,1], label='g=1', lw=2, marker='o', markersize=5, color='blue')
