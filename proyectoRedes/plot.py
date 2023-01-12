3da0f6713d0ed101940298f8877f6e8a67abf08c
AitorSanJose1 <aitorsanjoseheras@gmail.com>
2023-01-12 21:18:47 +0100

    asdaññ

--- proyectoRedes/plot.py
+++ proyectoRedes/plot.py
@@ -2,15 +2,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-d=np.loadtxt('epochplot.txt')
+d=np.loadtxt('lrplot.txt')
 fig, ax = plt.subplots()
-ax.set_title('tasa de error segun el numero de epocas');
+ax.set_title('tasa de error segun el learning-rate');
 ax.grid();
-ax.set_xlim([0,31]);
-ax.set_xlabel('Epoch');
-ax.set_ylim([7,20]);
+ax.set_xscale('log');
+ax.set_xlim([1.5e-6,1]);
+ax.set_xlabel('Learning-rate');
+ax.set_ylim([7,75]);
 ax.set_ylabel('error de clasificación');
 ax.plot(d[:,0], d[:,1], lw=2, marker='o', markersize=5, color='blue')
 ax.legend()
-plt.savefig('epochplot.pdf');
+plt.savefig('lrplot.pdf');
 plt.show();
